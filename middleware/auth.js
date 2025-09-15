6e8b88215700c964a55d6b741cbdb29a50884715
naxsyambis <naxsyambis@gmail.com>
2025-09-16 02:18:10 +0700

    menambahkan tampilan

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,31 +1,30 @@
-// middleware/auth.js 
-const jwt = require("jsonwebtoken"); 
- 
-module.exports = function (req, res, next) { 
- // Ambil token dari header 
- const authHeader = req.header("Authorization"); 
- 
- // Cek jika tidak ada header Authorization 
- if (!authHeader) { 
-   return res.status(401).json({ message: "No token, authorization denied" 
-}); 
- } 
- 
- // Cek jika formatnya bukan 'Bearer <token>' 
- const token = authHeader.split(" ")[1]; 
- if (!token) { 
-   return res 
-     .status(401) 
-     .json({ message: "Token format is incorrect, authorization denied" }); 
- } 
- 
-try { 
-// Verifikasi token 
-const decoded = jwt.verify(token, "your_super_secret_jwt_key"); // Gunakan secret key yang sama 
-// Tambahkan user dari payload token ke object request 
-req.user = decoded; 
-next(); 
-} catch (e) { 
-res.status(400).json({ message: "Token is not valid" }); 
-} 
-}; 
+const jwt = require("jsonwebtoken");
+
+module.exports = function (req, res, next) {
+    //ambil tooken dari header
+    const authHeader = req.header("Authorization");
+
+    //cek klo gk ada header authorization
+    if (!authHeader) {
+        return res.status(401).json({ message: "No token, authorization denied" });
+    }
+
+    //cek klo formatnya bukan "Bearer <token>"
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+        return res
+        .status(401)
+        .json({ message: "Token format is incorrect, authorization denied" });
+    }
+
+    try {
+        //verifikasi token
+        const decoded = jwt.verify(token, "your_super_secret_jwt_key");
+        
+        //tambahin user dari playload token ke object request
+        req.user = decoded;
+        next();
+    } catch (e) {
+        res.status(400).json({ message: "Token is not valid" });
+    }
+};
